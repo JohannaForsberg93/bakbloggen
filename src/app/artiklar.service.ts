952fc6c0bdd9d562e71e2a3926504f0336c167a2
JohannaForsberg93 <johanna_forsberg@msn.com>
2020-04-28 12:55:20 +0200

    Sista småändringar

--- src/app/artiklar.service.ts
+++ src/app/artiklar.service.ts
@@ -1,35 +1,38 @@
-import { Injectable } from '@angular/core';
-import { Artiklar } from './artiklar';
+import { Injectable } from "@angular/core";
+import { Artiklar } from "./artiklar";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ArtiklarService {
   static readonly ARTIKLAR: string = "artiklar";
-    artiklar = [new Artiklar("titel1", "content1", "author1", 1),
-    new Artiklar("t2", "c2", "a2", 1),
-    new Artiklar("t3,", "c3", "a3", 1)
-    ]
-    
-  constructor() { }
+  artiklar = [
+    new Artiklar("Title", "content", "author", 1),
+    new Artiklar("Title", "content", "author", 1),
+    new Artiklar("Title,", "content", "author", 1),
+  ];
 
-  public getArtiklar(){
+  constructor() {}
+
+  public getArtiklar() {
     let artiklarJSON = localStorage.getItem(ArtiklarService.ARTIKLAR);
     if (artiklarJSON) {
       this.artiklar = JSON.parse(artiklarJSON);
     }
     return this.artiklar;
   }
-  public sparaArtikel(artikel: Artiklar){
+  public sparaArtikel(artikel: Artiklar) {
     this.artiklar.unshift(artikel);
-    localStorage.setItem(ArtiklarService.ARTIKLAR, JSON.stringify(this.artiklar));
+    localStorage.setItem(
+      ArtiklarService.ARTIKLAR,
+      JSON.stringify(this.artiklar)
+    );
   }
-  public deleteArtikel(idx){
-    
+  public deleteArtikel(idx) {
     this.artiklar.splice(idx, 1);
-    localStorage.setItem(ArtiklarService.ARTIKLAR, JSON.stringify(this.artiklar));
+    localStorage.setItem(
+      ArtiklarService.ARTIKLAR,
+      JSON.stringify(this.artiklar)
+    );
   }
-  
 }
-  
-  
